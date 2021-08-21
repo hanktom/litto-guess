39a013321873a3d6de3c02f331dff4352b085c4d
Hank Tom <hanktom@gmail.com>
2021-08-21 21:47:31 +0800

    guess number

--- app/src/main/java/com/tom/guess/MainActivity.kt
+++ app/src/main/java/com/tom/guess/MainActivity.kt
@@ -3,24 +3,35 @@ package com.tom.guess
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 //Controller
 class MainActivity : AppCompatActivity() {
+
+    private val message: TextView?
+        get() {
+            val tv = findViewById<TextView>(R.id.message)
+            return tv
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //how to control views appearance
-        val tv = findViewById<TextView>(R.id.message)
-        tv.text = "Hello Kotlin!"
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            tv.text = "What?"
-        }
+        val guessButton = findViewById<Button>(R.id.guessButton)
         val reset = findViewById<Button>(R.id.reset)
+        guessButton.setOnClickListener {
+            val num = findViewById<EditText>(R.id.number)
+                .text.toString().toInt()
+            println(num)
+        }
         reset.setOnClickListener {
-            tv.text = "Hello Kotlin!"
-            button.isEnabled = false
         }
     }
+
+    fun test() {
+
+    }
+
 }
