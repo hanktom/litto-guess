db3f6f1d57604f14e53bea20fdccfa5c3dcf59fe
Hank Tom <hanktom@gmail.com>
2021-08-25 21:28:52 +0800

    Refactoring v1

--- app/src/main/java/com/tom/guess/MainActivity.kt
+++ app/src/main/java/com/tom/guess/MainActivity.kt
@@ -14,49 +14,43 @@ import java.lang.NumberFormatException
 //Controller
 class MainActivity : AppCompatActivity() {
 
-    private val message: TextView?
-        get() {
-            val tv = findViewById<TextView>(R.id.message)
-            return tv
-        }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //view-binding
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //how to control views appearance
         binding.guessButton.setOnClickListener {
-            val s = binding.number.text.toString()
-            //挑戰，在使用者有輸入資料後，按鈕才 Enable
-            if (s.length == 0) {
-                AlertDialog.Builder(this)
-                    .setTitle("使用提示")
-                    .setMessage("請輸入1-10的數字")
-                        //Positive, Negative, Neutral
-                    .setPositiveButton("OK", null)
-                    .show()
-            } else {
-                try {
-                    val num = s.toInt()
-                    Log.d("MainActivity", "num $num")
-                } catch(e: NumberFormatException) {
-                    //message
-                }
+            guess()
+        }
+        binding.reset.setOnClickListener {
+            reset()
+        }
+    }
+
+    private fun guess() {
+        val s = binding.number.text.toString()
+        //挑戰，在使用者有輸入資料後，按鈕才 Enable
+        if (s.length == 0) {
+            AlertDialog.Builder(this)
+                .setTitle("使用提示")
+                .setMessage("請輸入1-10的數字")
+                //Positive, Negative, Neutral
+                .setPositiveButton("OK", null)
+                .show()
+        } else {
+            try {
+                val num = s.toInt()
+                Log.d("MainActivity", "num $num")
+            } catch (e: NumberFormatException) {
+                //message
             }
         }
-        /*binding.reset.setOnClickListener {
-
-        }*/
-//        reset.setOnClickListener {
-//        }
     }
-    fun reset(view: View) {
+
+    fun reset() {
         Log.d("MainActivity", "reset: ")
     }
-
-    fun test() {
-
-    }
-
 }
